fa8555285ad896a310e117841ecda7df7cd8a436
olegyakovleviu5 <34161657+olegyakovleviu5@users.noreply.github.com>
2017-12-01 16:55:22 +0300

    Add files via upload

--- arr_algs.py
+++ arr_algs.py
@@ -17,37 +17,7 @@ def avg(arr):
     summ /= len(arr)
     return summ
 
-
-def qsort(arr):
-    qsort_rec(arr, 0, len(arr) - 1)
-
-
-def qsort_rec(arr, l, r):
-    if l >= r:
-        return
-    q = partition(arr, l, r);
-    qsort_rec(arr, l, q-1);
-    qsort_rec(arr, q+1, r);
-
-
-def partition(arr, l, r):
-    rq = random.randint(l, r);
-    arr[rq], arr[r] = arr[r], arr[rq]
-
-    q = l
-
-    for u in range(l, r):
-        if arr[u] < arr[r]:
-            arr[q], arr[u] = arr[u], arr[q]
-            q += 1
-
-    arr[r], arr[q] = arr[q], arr[r]
-
-    return q
-
-
 array = [4, 7, 8, 1, 3, 0, 3, -5, 1];
+print(array)
 print(arr_min(array))
 print(avg(array))
-qsort(array)
-print(array)
